662eec1f221c94a680595db28bdd242e05797f1e
Eric van Rees <ericvanrees@gmail.com>
2025-10-26 11:09:56 +0100

    add comment to ch3 main.ts file

--- 03 Chapter 3/src/main.ts
+++ 03 Chapter 3/src/main.ts
@@ -74,6 +74,9 @@ type Guitarist {
 
 */
 
+/* 
+Interfaces are a feature of TypeScript that allows us to define the structure or shape of an object and specify the properties and methods that an object has or should have. Their primary function is type checking and aiding developers in catching type-related errors during development.
+*/
 interface Guitarist {
   name?: string,
   active?: boolean,
