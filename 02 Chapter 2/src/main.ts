d4bdf2c3f67832e27829d50905b36b6efb922941
Eric van Rees <ericvanrees@gmail.com>
2025-10-26 09:46:33 +0100

    add comments to ch2 JS file

--- 02 Chapter 2/src/main.ts
+++ 02 Chapter 2/src/main.ts
@@ -1,9 +1,11 @@
-// explicitly declare that myName is a string:
+// TypeScript has 3 primitive types: number , boolean and string , same as in JavaScript. According to official TypeScript docs: we should always use lowercase names for primitive types, as Number, Boolean and String are special built-in types. //
+
+// how to explicitly declare that myName is a string: use column and specifiy data type
 let myName: string
 let meaningOfLife: number
 let isLoading: boolean
-// let album: any is also a possibility
-// "string | number" is a union type
+// let album: any is also a possibility, as "any" enables using any type of value
+// "string | number" is a union type, so either string or number type would be OK:
 let album: string | number
 
 myName = 'John'
@@ -12,11 +14,13 @@ isLoading = true
 album = 1984
 
 // you need to declare the variable type in a function
+// if not, TS implicitly will assign a and b to be of  "any" type
+// if you assign a to a number and b to a string, function will still return a string
 const sum = (a: number, b: number) => {
   return a + b
 }
 
 let postId: string | number
 let isActive: number | boolean 
-
+// regex is also a TS data type
 let re: RegExp  = /\w+/g
